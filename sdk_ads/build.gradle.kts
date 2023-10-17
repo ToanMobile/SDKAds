dca0badf9d28fb7b921f9377b7f27ac8ddaffa95
Toàn Huỳnh Văn <huynhvantoan.itc@gmail.com>
2023-10-17 09:36:19 +0700

    Update build.gradle.kts

--- sdk_ads/build.gradle.kts
+++ sdk_ads/build.gradle.kts
@@ -42,7 +42,7 @@ afterEvaluate {
                 from(components.findByName("release"))
                 groupId = "com.magic.sdk"
                 artifactId = "AdsSdk"
-                version = "v1.1.1"
+                version = "v1.1.2"
             }
         }
     }
