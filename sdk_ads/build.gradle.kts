aa3efef94de7bbce831385c105488a6af2d2df0b
ToanMobile <huynhvantoan.itc@gmail.com>
2024-02-01 09:25:43 +0700

    update sdk 2.0.0

--- sdk_ads/build.gradle.kts
+++ sdk_ads/build.gradle.kts
@@ -42,7 +42,7 @@ afterEvaluate {
                 from(components.findByName("release"))
                 groupId = "com.magic.sdk"
                 artifactId = "AdsSdk"
-                version = "v1.1.8"
+                version = "v2.0.0"
             }
         }
     }
