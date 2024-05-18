ed8731279e6d58193deb1e9c0ec06e1cba41be37
Huỳnh Văn Toàn <huynhvantoan.itc@gmail.com>
2024-05-18 15:03:01 +0700

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.googleService) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
