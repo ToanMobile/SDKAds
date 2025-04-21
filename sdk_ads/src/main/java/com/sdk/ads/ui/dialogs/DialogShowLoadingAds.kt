af7ced2d26051efe25304e42952ef4164c35cd42
Huỳnh Văn Toàn <hvtoan.dev@gmail.com>
2025-04-21 13:32:08 +0700

    Update DialogShowLoadingAds.kt

--- sdk_ads/src/main/java/com/sdk/ads/ui/dialogs/DialogShowLoadingAds.kt
+++ sdk_ads/src/main/java/com/sdk/ads/ui/dialogs/DialogShowLoadingAds.kt
@@ -2,11 +2,10 @@ package com.sdk.ads.ui.dialogs
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.ViewGroup
 import com.sdk.ads.databinding.DialogLoadingAdsBinding
 import com.sdk.ads.ui.BaseDialog
 
-class DialogShowLoadingAds(context: Context) : BaseDialog<DialogLoadingAdsBinding>(context, height = ViewGroup.LayoutParams.WRAP_CONTENT) {
+class DialogShowLoadingAds(context: Context) : BaseDialog<DialogLoadingAdsBinding>(context) {
 
     override val binding: DialogLoadingAdsBinding = DialogLoadingAdsBinding.inflate(LayoutInflater.from(context))
 
